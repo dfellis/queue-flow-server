e1461d69febdc7ad4f23bf4a47c644f68f9cd354
David Ellis <d.f.ellis@ieee.org>
2013-02-05 07:44:29 -0800

    Updating the queue-flow-server bin file to match the current readme, adding a new package for merging objects, and updating gitignore.

--- bin/queue-flow-server.js
+++ bin/queue-flow-server.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var commander = require('commander');
+var deepmerge = require('deepmerge');
 var qfserver = require('../lib/server');
 
 // Parse command-line options
@@ -10,16 +11,33 @@ commander
 
 // Define the default config
 var config = {
-    port: 11235 
+    transports: {},
+    isMaster: true,
+    processes: 4, // Ignored for now
+    stats: {
+        enabled: true
+    },
+    modules: {
+        q: 'queue-flow>=0.5.33',
+        parallel: 'parallel-queue-flow',
+        sloppy: 'sloppy-queue-flow',
+        l: 'lambda-js'
+    }
 };
 
 // Override the defaults if an alternate config file is specified
 if(commander.config) {
-    var newConfig = fs.readFileSync(commander.config, 'utf8');
-    for(var key in newConfig) {
-        config[key] = newConfig[key];
+    try {
+        var newConfig = JSON.parse(fs.readFileSync(commander.config, 'utf8'));
+        config = deepmerge(config, newConfig);
+    } catch(e) {
+        console.err('Could not read the config file. ' + e.message);
+        process.exit(-1);
     }
 }
+
+// Add the REST interface if none was specified
+if(Object.keys(config.transports).length === 0) config.transports.rest = 11235;
 
 // Start the server
 qfserver(config);
